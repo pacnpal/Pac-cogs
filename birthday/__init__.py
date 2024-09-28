51e5b580228d9e431c6e514618935e8d68bab485
pacnpal <pac@pacnp.al>
2024-09-28 17:35:03 -0400

    Update __init__.py

--- birthday/__init__.py
+++ birthday/__init__.py
@@ -3,5 +3,5 @@ from .birthday import Birthday
 __red_end_user_data_statement__ = "This allows users with the set roles to give the birthday role to users until the end of the day."
 
 
-def setup(bot):
+async def setup(bot):
     await bot.add_cog(Birthday(bot))
