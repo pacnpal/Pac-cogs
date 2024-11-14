a0b3a56d4fb4c8964ac316a8f79ffc8a2f4461f3
pacnpal <183241239+pacnpal@users.noreply.github.com>
2024-11-14 22:37:59 +0000

    fix: Update cog initialization
    - Added proper Red-DiscordBot cog setup
    - Added end user data statement
    - Improved module organization

--- videoarchiver/__init__.py
+++ videoarchiver/__init__.py
@@ -1,1 +1,11 @@
 """VideoArchiver cog for Red-DiscordBot"""
+
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+
+from .video_archiver import VideoArchiver
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+
+async def setup(bot: Red):
+    await bot.add_cog(VideoArchiver(bot))
