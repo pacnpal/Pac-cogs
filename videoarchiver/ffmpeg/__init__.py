dfba1a4e3062f1eb9037a9fb62e4aa2ef26dc6bc
pacnpal <183241239+pacnpal@users.noreply.github.com>
2024-11-14 23:05:16 +0000

    fix imports

--- videoarchiver/ffmpeg/__init__.py
+++ videoarchiver/ffmpeg/__init__.py
@@ -1,6 +1,6 @@
 """FFmpeg management package"""
 
-from videoarchiver.ffmpeg.exceptions import FFmpegError, GPUError, DownloadError
-from videoarchiver.ffmpeg.ffmpeg_manager import FFmpegManager
+from .exceptions import FFmpegError, GPUError, DownloadError
+from .ffmpeg_manager import FFmpegManager
 
 __all__ = ['FFmpegManager', 'FFmpegError', 'GPUError', 'DownloadError']
--- videoarchiver/ffmpeg_manager.py
+++ videoarchiver/ffmpeg_manager.py
@@ -1,7 +1,7 @@
 """FFmpeg management module"""
 
-# Import directly from the local ffmpeg package
-from videoarchiver.ffmpeg.ffmpeg_manager import FFmpegManager
-from videoarchiver.ffmpeg.exceptions import FFmpegError, GPUError, DownloadError
+# Use relative imports since we're within the package
+from .ffmpeg.ffmpeg_manager import FFmpegManager
+from .ffmpeg.exceptions import FFmpegError, GPUError, DownloadError
 
 __all__ = ['FFmpegManager', 'FFmpegError', 'GPUError', 'DownloadError']
