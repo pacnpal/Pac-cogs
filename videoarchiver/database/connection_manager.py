bdc1ce4f4ca08cdf70a95b32dfb040adfbc196f9
pacnpal <183241239+pacnpal@users.noreply.github.com>
2024-11-16 17:41:25 +0000

    Rename ConnectionManager to DatabaseConnectionManager to fix import error

--- videoarchiver/database/connection_manager.py
+++ videoarchiver/database/connection_manager.py
@@ -10,7 +10,7 @@ from queue import Queue, Empty
 
 logger = logging.getLogger("DBConnectionManager")
 
-class ConnectionManager:
+class DatabaseConnectionManager:
     """Manages SQLite database connections and connection pooling"""
 
     def __init__(self, db_path: Path, pool_size: int = 5):
